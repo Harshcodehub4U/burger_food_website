3c1943d1aaca761c092c6217d64fb577fe1cc1e2
Harsh <165124059+Harshcodehub4U@users.noreply.github.com>
2024-09-20 10:49:24 +0530

    Add files via upload

---
+++ food website/script.js
@@ -0,0 +1,17 @@
+var typed = new Typed('.typer', {
+    strings: ['so fresh, you will think it is been squeezed by an angel', 'Sip, sip, hooray! Another glass of healthy goodness', "Keep calm and drink fresh juice."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    loopCount: Infinity,
+    
+
+});
+var typed = new Typed('.typer1', {
+    strings: ['Spice is life', 'Can you handle the heat', "The spicier, the better"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    loopCount: Infinity,
+    
+});
